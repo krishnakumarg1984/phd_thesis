961ea1b0a6adc2ccb744f7697e7b6d48d154f038
Krishnakumar <krishnakumar@imperial.ac.uk>
2018-06-06 21:16:24 +0100

    more robus fixes pertaining to glossaries acronymns definitions

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
+chmod +x ./dropbox_uploader.sh
+chmod +x ./texlive_install.sh
+
+exit 0
