246d2c913a2097c6513f2a41d942ab664e7e55a4
Gopalakrishnan <kg314@ic.ac.uk>
2018-12-31 23:45:10 +0000

    complete reorganisaton of the thesis folder structure into frontmatter, mainmatter & backmatter

--- diff_prep_script.sh
+++ diff_prep_script.sh
@@ -18,29 +18,8 @@ cp --parents `find -name \*.otf` ../diff_head_submitted/
 cp --parents `find -name \*.m` ../diff_head_submitted//
 rm -rf ../diff_head_submitted/notes*
 
-cp chapters/frontmatter/figures/black_ink_sign_from_jpg.pdf ../diff_head_submitted/
-cp chapters/frontmatter/figures/narayam_sanskrit.pdf ../diff_head_submitted/
-cp chapters/frontmatter/figures/doclicense-CC-by-nc-nd.pdf ../diff_head_submitted/chapters/backmatter/
-
-# find . -name \*.pdf -exec cp {} ../diff_head_submitted \;
-# find . -name \*.PNG -exec cp {} ../diff_head_submitted \;
-# find . -name \*.png -exec cp {} ../diff_head_submitted \;
-# find . -name \*.jpg -exec cp {} ../diff_head_submitted \;
-# find . -name \*.bib -exec cp {} ../diff_head_submitted \;
-
-# [ -d ../head_flattened_phd_thesis ] || mkdir ../head_flattened_phd_thesis
-# latexpand -o ../head_flattened_phd_thesis/flattened_head.tex --show-graphics --makeatletter main.tex
-
-# cp latexmkrc ../head_flattened_phd_thesis/
-
-# cp --parents `find -name \*.xdy` ../head_flattened_phd_thesis/ 2>/dev/null || : # If you want to suppress the exit code and the error message: https://serverfault.com/questions/153875/how-to-let-cp-command-dont-fire-an-error-when-source-file-does-not-exist
-# cp --parents `find -name \*.PNG` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.png` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.jpg` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.pdf` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.bib` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.icc` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.otf` ../head_flattened_phd_thesis/
-# cp --parents `find -name \*.m` ../head_flattened_phd_thesis/
-# rm -rf ../head_flattened_phd_thesis/notes*
+cp frontmatter/project_outputs.pdf ../diff_head_submitted/
+cp frontmatter/figures/black_ink_sign_from_jpg.pdf ../diff_head_submitted/
+cp frontmatter/figures/narayam_sanskrit.pdf ../diff_head_submitted/
+cp frontmatter/figures/doclicense-CC-by-nc-nd.pdf ../diff_head_submitted/chapters/backmatter/
 
