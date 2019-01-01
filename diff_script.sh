41e042364e5c26722df3246ac5d19d44eb873492
Gopalakrishnan <kg314@ic.ac.uk>
2019-01-01 17:08:03 +0000

    renamed diff script. better shebang line. force checkout. platform check for gnu sed

--- diff_script.sh
+++ diff_script.sh
@@ -1,18 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# If from windows, run this script from only cmder's version of cmd/powershell (not from native cmd, native powershell, mintty-cygwin, mintty-mingw or git-bash). This is because call to xindy shall fail otherwise
 if [ ! -d ../nonflat_submitted ]; then
     cd ..
     cp -R phd_thesis/ nonflat_submitted/
     cd nonflat_submitted/
-    git checkout 383e7c8 # with gitinfo2 footer
-    # git checkout 0f26b11 # without footer
+    git checkout -f 383e7c8 # with gitinfo2 footer. The -f flag helps to force checkout to facilitate local uncommitted changes to the repo (like testing this script)
+    # git checkout 0f26b11 # without footer.  The -f flag helps to force checkout to facilitate local uncommitted changes to the repo (like testing this script)
     cd ../phd_thesis/
 fi
 
 if [ ! -d ../diff_head_submitted ]; then
     mkdir ../diff_head_submitted
     cp latexmkrc ../diff_head_submitted/
-    sed -i 's/main/diff/g' ../diff_head_submitted/latexmkrc
+    if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] || [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
+        H:\portable_win_utilities\sed.exe -i "s/main/diff/g" ..\diff_head_submitted\latexmkrc
+    else
+        sed -i 's/main/diff/g' ../diff_head_submitted/latexmkrc
+    fi
 fi
 
 # latexdiff -c ld.cfg  --driver=pdftex --subtype=ONLYCHANGEDPAGE --floattype=IDENTICAL --verbose --flatten --math-markup=3 --graphics-markup=0 --enable-citation-markup -L submitted_version -L latest_version ../nonflat_submitted/main.tex main.tex > ../diff_head_submitted/diff_changes.tex
