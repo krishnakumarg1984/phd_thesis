f6f77e2b60b00c8dc8665bb44131620b15fae83c
Krishnakumar Gopalakrishnan <krishnak@vt.edu>
2019-01-01 18:12:43 +0000

    confused with handling the diff script on windows

--- diff_script.sh
+++ diff_script.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# If from windows, run this script from only cmder's version of cmd/powershell (not from native cmd, native powershell, mintty-cygwin, mintty-mingw or git-bash). This is because call to xindy shall fail otherwise
 if [ ! -d ../nonflat_submitted ]; then
     cd ..
     cp -R phd_thesis/ nonflat_submitted/
@@ -20,21 +19,18 @@ if [ ! -d ../diff_head_submitted ]; then
     fi
 fi
 
-# latexdiff -c ld.cfg  --driver=pdftex --subtype=ONLYCHANGEDPAGE --floattype=IDENTICAL --verbose --flatten --math-markup=3 --graphics-markup=0 --enable-citation-markup -L submitted_version -L latest_version ../nonflat_submitted/main.tex main.tex > ../diff_head_submitted/diff_changes.tex
-
-latexdiff -c ld.cfg --driver=pdftex --floattype=IDENTICAL --verbose --flatten --math-markup=3 --graphics-markup=0 --enable-citation-markup -L submitted_version -L latest_version ../nonflat_submitted/main.tex main.tex > ../diff_head_submitted/diff.tex
+cp --parents `find -name \*.PNG` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.png` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.jpg` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.pdf` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.bib` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.icc` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.otf` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+cp --parents `find -name \*.m` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message
+rm -rf ../diff_head_submitted/notes*
 
 cp --parents `find -name \*.xdy` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message: https://serverfault.com/questions/153875/how-to-let-cp-command-dont-fire-an-error-when-source-file-does-not-exist
 
-cp --parents `find -name \*.PNG` ../diff_head_submitted/
-cp --parents `find -name \*.png` ../diff_head_submitted/
-cp --parents `find -name \*.jpg` ../diff_head_submitted/
-cp --parents `find -name \*.pdf` ../diff_head_submitted/
-cp --parents `find -name \*.bib` ../diff_head_submitted/
-cp --parents `find -name \*.icc` ../diff_head_submitted/
-cp --parents `find -name \*.otf` ../diff_head_submitted/
-cp --parents `find -name \*.m` ../diff_head_submitted//
-rm -rf ../diff_head_submitted/notes*
 
 cp frontmatter/project_outputs.pdf ../diff_head_submitted/
 cp frontmatter/coverpage.pdf ../diff_head_submitted/
@@ -42,10 +38,16 @@ cp frontmatter/figures/black_ink_sign_from_jpg.pdf ../diff_head_submitted/
 cp frontmatter/figures/narayam_sanskrit.pdf ../diff_head_submitted/
 cp frontmatter/figures/doclicense-CC-by-nc-nd.pdf ../diff_head_submitted/chapters/backmatter/
 
+latexdiff -c ld.cfg --driver=pdftex --floattype=IDENTICAL --verbose --flatten --math-markup=3 --graphics-markup=0 --enable-citation-markup -L submitted_version -L latest_version ../nonflat_submitted/main.tex main.tex > ../diff_head_submitted/diff.tex
+
 cd ../diff_head_submitted
-latexmk -f --shell-escape -halt-on-error diff.tex
-latexmk -f --shell-escape diff.tex
-# latexmk -f --shell-escape -halt-on-error diff_changes.tex
-# latexmk -f --shell-escape diff_changes.tex
+
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] || [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
+    start cmd /c ""latexmk -f --shell-escape -halt-on-error diff.tex"" & ""latexmk -f --shell-escape diff.tex""
+else
+    latexmk -f --shell-escape -halt-on-error diff_changes.tex
+    latexmk -f --shell-escape diff_changes.tex
+fi
+
 cd ../phd_thesis
 
