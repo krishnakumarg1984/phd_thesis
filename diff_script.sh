cc95233974becc0baab55b970a15ccc2d7991758
Krishnakumar Gopalakrishnan <krishnak@vt.edu>
2019-01-09 15:43:48 +0000

    cleans up windows specific portions of diff script

--- diff_script.sh
+++ diff_script.sh
@@ -30,32 +30,24 @@ rm -rf ../diff_head_submitted/main*
 
 cp --parents `find -name \*.xdy` ../diff_head_submitted/ 2>/dev/null || : # If you want to suppress the exit code and the error message: https://serverfault.com/questions/153875/how-to-let-cp-command-dont-fire-an-error-when-source-file-does-not-exist
 
-
 cp frontmatter/project_outputs.pdf ../diff_head_submitted/
 cp frontmatter/coverpage.pdf ../diff_head_submitted/
 cp frontmatter/figures/black_ink_sign_from_jpg.pdf ../diff_head_submitted/
 cp frontmatter/figures/narayam_sanskrit.pdf ../diff_head_submitted/
 cp run_diff_latexmk.bat ../diff_head_submitted/
 
-
 latexdiff -c ld.cfg --driver=pdftex --floattype=IDENTICAL --packages=amsmath,hyperref,siunitx,cleveref,glossaries,chemformula --verbose --flatten --math-markup=3 --graphics-markup=0 --enable-citation-markup -L submitted_version -L latest_version -p Preamble/ltxdiff_defaultstyle_preamble.tex ../nonflat_submitted/main.tex main.tex > ../diff_head_submitted/diff.tex
 
 # latexdiff -c ld.cfg --driver=pdftex --floattype=IDENTICAL --packages=amsmath,hyperref,siunitx,cleveref,glossaries,chemformula --exclude-textcmd="section,subsection" --verbose --flatten --math-markup=3 --graphics-markup=0 --enable-citation-markup -L submitted_version -L latest_version  ../nonflat_submitted/main.tex main.tex > ../diff_head_submitted/diff.tex
 
-
 cd ../diff_head_submitted
-
-
-# sed -i '/RequirePackage\[normalem\]/d' diff.tex
-# sed -i '/RequirePackage{color}/d' diff.tex
-# sed -i 's/{red}/{imperialbrick}/g'  diff.tex
-# sed -i 's/{blue}/{imperialblue}/g'  diff.tex
-# sed -i 's/\\uwave.*}/}/g' diff.tex
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] || [ "$(expr substr $(uname -s) 1 7)" == "MSYS_NT" ]; then
     # :
     # ../phd_thesis/run_diff_latexmk.bat  # https://stackoverflow.com/questions/11865085/out-of-a-git-console-how-do-i-execute-a-batch-file-and-then-return-to-git-conso
-    start cmd "/C run_diff_latexmk.bat"
+    # start cmd "/C run_diff_latexmk.bat"
+    latexmk -C diff.tex
+    latexmk -f --shell-escape diff.tex
 else
     if [ -f diff_changedpages.tex ]; then
         latexmk -C diff_changedpages.tex
